661222eca298c7a26a764e369e62718b0f63f6e1
liujiahe971024@gmail.com <liujiahe971024@gmail.com>
2021-11-14 22:30:33 +0800

    Rename

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/thousandDream'),
+      dir: require('path').join(__dirname, './coverage/FiatLux'),
       subdir: '.',
       reporters: [
         { type: 'html' },
